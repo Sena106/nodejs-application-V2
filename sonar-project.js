0b9df11a8c09568913e5f6c0f696f7096ba892ff
Sena106 <127817999+Sena106@users.noreply.github.com>
2023-05-13 20:10:09 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://23.22.244.182:8000/',
+  serverUrl: 'http://172.31.80.15:8000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '73e279fcb46164fb832e53de6d23af5bc2f783f9',
+	    'sonar.login': '69390a34c224b99bea7a9f5ec54d078628a78d68',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
